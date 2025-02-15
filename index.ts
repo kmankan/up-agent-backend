a6d2e4def60db171058346c44b98e5ebd734b804
mahlen <malink027@gmail.com>
2025-02-15 22:48:44 +1100

    cors

--- index.ts
+++ index.ts
@@ -36,7 +36,7 @@ const startServer = async () => {
 
     // Start server
     app.listen(PORT, () => {
-      console.log(`Server is running at ${process.env.RAILWAY_PUBLIC_DOMAIN}`);
+      console.log(`Server is running at ${process.env.NEXT_PUBLIC_SERVER_URL}`);
     });
   } catch (error) {
     console.error('Failed to start server:', error);
